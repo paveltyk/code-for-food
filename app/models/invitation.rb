88b2f063afb5c988d171d925794b739c751ef793
PavelT <paveltyk@gmail.com>
2011-02-17 13:29:41 +0200

    Invitation now uses email regexp from authlogic

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,13 +1,11 @@
 class Invitation < ActiveRecord::Base
-  RE_EMAIL = /^[A-Z0-9_\.%\+\-]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,4}|museum|travel)$/i
-
   belongs_to :sender, :class_name => 'User'
   has_one :receiver, :class_name => 'User'
 
   before_create :generate_token
   validates_presence_of :recipient_email, :sender
   validates_uniqueness_of :recipient_email, :if => 'recipient_email.present?'
-  validates_format_of :recipient_email, :with => RE_EMAIL, :message => "must be a valid email", :if => 'recipient_email.present?'
+  validates_format_of :recipient_email, :with => Authlogic::Regex.email, :message => "must be a valid email", :if => 'recipient_email.present?'
 
   private
 
