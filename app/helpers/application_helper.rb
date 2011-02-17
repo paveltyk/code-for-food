edefd5cdabeea21c4edf309125507772c27be172
PavelT <paveltyk@gmail.com>
2011-02-17 13:46:26 +0200

    ApplicationHelper#render_flash_messages added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,12 @@ module ApplicationHelper
       end
     end.html_safe
   end
+
+  def render_flash_messages
+    return '' if flash.blank?
+    html = ''
+    flash.each { |key, msg| html << content_tag(:div, msg, :class => key) }
+    content_tag :div, html.html_safe, :class => 'flash'
+  end
 end
 
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,5 +8,22 @@ describe ApplicationHelper do
       helper.render_errors_for(model).should match /<p[^>]*>Error 1<\/p><p[^>]*>Error 2<\/p>/i
     end
   end
+
+  describe "#render_flash_messages" do
+    it "returns blank string if no flash" do
+      flash.should be_blank
+      helper.render_flash_messages.should eql('')
+    end
+
+    it "returns div with class 'flash'" do
+      flash[:notice] = 'Hello'
+      helper.render_flash_messages.should match(/^<div class="flash">.*?<\/div>$/i)
+    end
+
+    it "returns div with class 'notice'" do
+      flash[:notice] = 'Hello'
+      helper.render_flash_messages.should match(/<div class="notice">Hello<\/div>/i)
+    end
+  end
 end
 
