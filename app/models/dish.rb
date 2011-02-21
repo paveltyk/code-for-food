d609ba98db77d7bedfedae99f41ab1f3afb3c67f
PavelT <paveltyk@gmail.com>
2011-02-21 12:57:34 +0200

    Relation Order to Dish are uniq

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,8 +1,8 @@
 class Dish < ActiveRecord::Base
   belongs_to :menu
   has_and_belongs_to_many :tags, :class_name => 'DishTag', :uniq => true
-  has_many :order_items, :dependent => :destroy, :inverse_of => :dish
-  has_many :orders, :through => :order_items
+  has_many :order_items, :dependent => :destroy, :inverse_of => :dish, :uniq => true
+  has_many :orders, :through => :order_items, :uniq => true
 
   validates_presence_of :name, :price, :menu
   validates_numericality_of :price, :only_integer => true, :greater_than_or_equal_to => 10
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :menu
-  has_many :order_items, :dependent => :destroy, :inverse_of => :order
-  has_many :dishes, :through => :order_items
+  has_many :order_items, :dependent => :destroy, :inverse_of => :order, :uniq => true
+  has_many :dishes, :through => :order_items, :uniq => true
 
   validates_presence_of :user, :menu
 end
