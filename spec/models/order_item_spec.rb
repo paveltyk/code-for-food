822053b5f6bef07b31f36aaf3e01458e59b788ce
PavelT <paveltyk@gmail.com>
2011-02-24 19:15:36 +0200

    Specs for order item improved

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -7,19 +7,31 @@ describe OrderItem do
     end
 
     it "not valid if dish is blank" do
-      OrderItem.make(:dish => nil).should_not be_valid
+      OrderItem.make(:dish => nil) do |oi|
+        oi.should_not be_valid
+        oi.should have_at_least(1).error_on(:dish)
+      end
     end
 
     it "not valid if order is blank" do
-      OrderItem.make(:order => nil).should_not be_valid
+      OrderItem.make(:order => nil) do |oi|
+        oi.should_not be_valid
+        oi.should have_at_least(1).error_on(:order)
+      end
     end
 
     it "not valid if quantity is less then 1" do
-      OrderItem.make(:quantity => 0).should_not be_valid
+      OrderItem.make(:quantity => 0) do |oi|
+        oi.should_not be_valid
+        oi.should have_at_least(1).error_on(:quantity)
+      end
     end
 
     it "not valid if quantity is not integer" do
-      OrderItem.make(:quantity => 1.5).should_not be_valid
+      OrderItem.make(:quantity => 1.5) do |oi|
+        oi.should_not be_valid
+        oi.should have_at_least(1).error_on(:quantity)
+      end
     end
   end
 end
