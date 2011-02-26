eabb46b7d2add5e1cfff88fba2f3573c3bc90235
PavelT <paveltyk@gmail.com>
2011-02-26 16:44:38 +0200

    Order calculates price after validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,8 @@ class Order < ActiveRecord::Base
 
   validates_presence_of :user, :menu
 
+  after_validation :calculate_price
+
   def menu_items
     menu.dishes.map do |dish|
       existent_oi = self.order_items.select{ |oi| oi.dish_id == dish.id }.first
@@ -16,6 +18,12 @@ class Order < ActiveRecord::Base
   def menu_items_attributes=(attributes)
     attributes = attributes.delete_if{ |k,v| v[:is_ordered] != '1' }.map { |attrs| attrs[1] }
     self.order_items = attributes.map { |attrs| self.order_items.new attrs }
+  end
+
+  private
+
+  def calculate_price
+    self.price = self.order_items.map{ |oi| oi.dish.price * oi.quantity }.sum
   end
 end
 
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -16,11 +16,7 @@ describe Order do
   end
 
   describe "menu_items" do
-    let(:menu) do
-      Menu.make!.tap do |menu|
-        3.times { menu.dishes << Dish.make! }
-      end
-    end
+    let(:menu) { Menu.make!(:with_3_dishes) }
     let(:order) { Order.make :menu => menu }
 
     it "generates 3 menu items" do
@@ -32,6 +28,7 @@ describe Order do
         order.menu_items.map(&:dish_id).should include(dish.id)
       end
     end
+
     describe " accepts nested attributes for menu_items" do
       let(:attrs_for_first_dish) { { :dish_id => menu.dishes.first.id, :quantity => 1, :is_ordered => '1'} }
 
@@ -43,6 +40,21 @@ describe Order do
       it "builds none order_items" do
         order.menu_items_attributes = { "0" => attrs_for_first_dish.merge(:is_ordered => '0') }
         order.order_items.should have(0).item
+      end
+    end
+
+    describe "price calculation" do
+      it "calculates price as sum of dish prices" do
+        menu.dishes.each { |dish| order.order_items.build :dish => dish }
+        order.valid?
+        order.price.should eql(menu.dishes.sum(:price))
+      end
+
+      it "uses quantity as multiplier" do
+        dish = menu.dishes.first
+        order.order_items.build :dish => dish, :quantity => 10
+        order.valid?
+        order.price.should eql(dish.price * 10)
       end
     end
   end
