4b194404f16ba05dad4cfc54bb96da6eb9d07b51
PavelT <paveltyk@gmail.com>
2011-02-28 20:48:41 +0200

    Specs fixed

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -12,11 +12,27 @@ describe UsersHelper do
       end
     end
 
-    context "when logged in" do
-      before(:each) { helper.stub :current_user => User.make }
+    context "when logged in as User" do
+      before(:each) { helper.stub :current_user => User.make, :is_admin? => false }
 
       it "renders logout link" do
         helper.user_nav.should match(/logout/i)
+      end
+    end
+
+    context "when logged in as Administrator" do
+      before(:each) { helper.stub :current_user => Administrator.make, :is_admin? => true }
+
+      it "renders new menu link" do
+        helper.user_nav.should match(new_menu_path)
+      end
+
+      it "renders tags link" do
+        helper.user_nav.should match(dish_tags_path)
+      end
+
+      it "renders invitations link" do
+        helper.user_nav.should match(new_invitation_path)
       end
     end
   end
