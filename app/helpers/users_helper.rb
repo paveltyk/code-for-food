b965cf0c384acaf5d9b6e64a88313a842e2fedef
PavelT <paveltyk@gmail.com>
2011-02-28 20:43:33 +0200

    user_menu changed a bit.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,17 @@
 module UsersHelper
   def user_nav
+    links = []
     if current_user
-      link_to 'Logout', logout_path
+      if is_admin?
+        links << link_to('Новое меню', new_menu_path)
+        links << link_to('Метки', dish_tags_path)
+        links << link_to('Новое приглашение', new_invitation_path)
+      end
+      links << link_to('Выйти', logout_path)
     else
-      link_to 'Login', login_path
+      links << link_to('Войти', login_path)
     end
+    content_tag :ul, links.map { |link| content_tag :li, link }.join(content_tag :li, '|', :class => 'separator').html_safe, :id => 'user-nav'
   end
 end
 
