06982814e5a31447dd16518b9a7dfdd6bc2bb136
PavelT <paveltyk@gmail.com>
2011-03-01 22:36:16 +0200

    render_tags helper method renders only one minus sign now!

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -2,7 +2,7 @@ module TagHelper
   def render_tags(tags)
     tags.map do |tag|
       text = tag.name.to_s
-      text << " #{tag.value > 0 ? '+' : '-'}#{number_with_delimiter tag.value}" if tag.value != 0
+      text << " #{'+' if tag.value > 0}#{number_with_delimiter tag.value}" if tag.value != 0
       content_tag :span, text, :title => tag.description, :class => 'tag'
     end.to_s.html_safe
   end
--- spec/helpers/tag_helper_spec.rb
+++ spec/helpers/tag_helper_spec.rb
@@ -5,6 +5,14 @@ describe TagHelper do
     it "renders 2 spans" do
       helper.render_tags([DishTag.make, DishTag.make]).scan(/<\/span>/).should have(2).items
     end
+
+    it "renders price with sign (minus)" do
+      helper.render_tags([DishTag.make :value => -100, :name => 'Hello']).should match(/Hello -100/i)
+    end
+
+    it "renders price with sign (plus)" do
+      helper.render_tags([DishTag.make :value => 100, :name => 'Hello']).should match(/Hello \+100/i)
+    end
   end
 end
 
