925fcaed6bdfda101dba9e5dfeb9461cacb8fbe0
PavelT <paveltyk@gmail.com>
2011-03-02 10:15:47 +0200

    Added some specs for TaggingObserver

--- spec/models/tagging_observer_spec.rb
+++ spec/models/tagging_observer_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe TaggingObserver do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { TaggingObserver.instance }
+  let(:dish) { Dish.make! :price => 1000}
+  let(:tag) {DishTag.make! :value => 100}
+
+  context "when tagging created" do
+    it "updates dish total_price" do
+      dish.tags << tag
+      dish.reload.total_price.should eql 1100
+    end
+  end
+
+  context "when tagging removed" do
+    it "updates dish total_price" do
+      dish.tags << tag
+      dish.taggings.find_by_dish_tag_id(tag.id).destroy
+      dish.reload.total_price.should eql 1000
+    end
+  end
 end
+
