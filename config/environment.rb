7526435b71184a0041026a783fa332686c095e79
PavelT <paveltyk@gmail.com>
2011-03-09 15:33:12 +0200

    smtp_sessings set.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 CodeForFood::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => '25',
+  :domain => "code-for-food.info",
+  :authentication => :plain,
+  :user_name => ENV['SG_LOGIN'],
+  :password => ENV['SG_PASS']
+}
+
