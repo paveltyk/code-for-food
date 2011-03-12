49926dbdbd878bb69678a95cfac30d200eb86c4a
PavelT <paveltyk@gmail.com>
2011-03-12 09:57:51 +0200

    Russified Mailer#invite subject and created #menu_published email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,7 +3,14 @@ class Mailer < ActionMailer::Base
     @invitation = invitation
     mail :to => invitation.recipient_email,
          :from => invitation.sender.email,
-         :subject => "Your invitation to join \"Code for Food\" meal ordering system"
+         :subject => 'Приглашение для регистрации на сайте code-for-food.info'
+  end
+
+  def menu_published(menu)
+    @menu = menu
+    mail :to => 'group-delivery@code-for-food.info',
+         :from => 'no-reply@code-for-food.info',
+         :subject => "Опубликовано меню на \"#{@menu}\""
   end
 end
 
--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -1,18 +1,33 @@
 require "spec_helper"
 
 describe Mailer do
-  describe "invitation" do
+  describe '#invitation' do
     let(:invitation) { Invitation.make! }
     let(:mail) { Mailer.invitation(invitation) }
 
-    it "renders the headers" do
-      mail.subject.should eq("Your invitation to join \"Code for Food\" meal ordering system")
-      mail.to.should eq([invitation.recipient_email])
-      mail.from.should eq([invitation.sender.email])
+    it 'renders the headers' do
+      mail.subject.should eq 'Приглашение для регистрации на сайте code-for-food.info'
+      mail.to.should eq [invitation.recipient_email]
+      mail.from.should eq [invitation.sender.email]
     end
 
-    it "renders the body" do
+    it 'renders registration link in the body' do
       mail.body.encoded.should match(register_url(:invitation_token => invitation.token))
+    end
+  end
+
+  describe '#menu_published' do
+    let(:menu) { Menu.make! }
+    let(:mail) { Mailer.menu_published(menu) }
+
+    it 'render the headers' do
+      mail.subject.should match menu.to_s
+      mail.to.should eq ['group-delivery@code-for-food.info']
+      mail.from.should eq ['no-reply@code-for-food.info']
+    end
+
+    it 'renders the menu link in the body' do
+      mail.body.encoded.should match new_order_url(menu)
     end
   end
 
