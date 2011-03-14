1ad0dd41ef78179335936d3df7a3039f567934a4
PavelT <paveltyk@gmail.com>
2011-03-14 12:23:28 +0200

    Added webrat gem

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'russian'
 
 group :development, :test do
   gem 'rspec-rails', '~> 2.5'
+  gem 'webrat'
   gem 'rcov', '~> 0.9'
   gem 'metric_fu', '~> 2.0'
   gem 'machinist', '~> 2.0.0.beta2'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'authlogic/test_case'
+require 'webrat'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
@@ -11,5 +12,9 @@ include Authlogic::TestCase
 RSpec.configure do |config|
   config.mock_with :rspec
   config.use_transactional_fixtures = false
+end
+
+Webrat.configure do |config|
+  config.mode = :rails
 end
 
