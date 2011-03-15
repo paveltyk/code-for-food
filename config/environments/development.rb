2283974e5d8e181104fccf97127bc284f262bc81
PavelT <paveltyk@gmail.com>
2011-03-15 18:53:00 +0200

    Mailer send method set to :test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,7 @@ CodeForFood::Application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 
   # Print deprecation notices to the Rails logger
