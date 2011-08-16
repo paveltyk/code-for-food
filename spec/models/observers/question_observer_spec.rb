2151c7b06fe997f6a9c96d924517ba80f6334880
PavelT <paveltyk@gmail.com>
2011-08-16 02:16:09 -0700

    Forum: added :receive_forum_notification functionality

--- spec/models/observers/question_observer_spec.rb
+++ spec/models/observers/question_observer_spec.rb
@@ -4,7 +4,8 @@ describe QuestionObserver do
   before(:each) { QuestionObserver.instance }
 
   describe 'after question posted' do
-    it 'sends out an email' do
+    it 'sends out an email to users who want to get forum notification' do
+      User.make! :receive_forum_notifications => false
       expect {
         Question.make!
       }.to change(ActionMailer::Base.deliveries, :count).by(1)
